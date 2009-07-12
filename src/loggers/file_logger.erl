7c5d1364ad6ac340a7b355837c4ca44c6306e779
Nicolas R Dufour <nrdufour@gmail.com>
2009-07-11 22:35:01 -0400

    Fix all those nasty warnings.

--- src/loggers/file_logger.erl
+++ src/loggers/file_logger.erl
@@ -13,7 +13,7 @@
 -module(file_logger).
 -behaviour(gen_event).
 
--export([init/1, handle_event/2, terminate/2]).
+-export([init/1, handle_event/2, terminate/2, handle_call/2, handle_cast/2, handle_info/2, code_change/3]).
 
 init(File) ->
 	{ok, Fd} = file:open(File, read),
@@ -31,6 +31,18 @@ handle_event(Msg, {Trace, Fd}) ->
 			true
 	end,
 	{ok, {Trace, Fd}}.
+
+handle_call(_, State) ->
+	{noreply, State}.
+
+handle_cast(_, State) ->
+	{noreply, State}.
+
+handle_info(_Info, State) ->
+	{noreply, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
 
 terminate(_Args, {_Trace, Fd}) ->
 	file:close(Fd).
