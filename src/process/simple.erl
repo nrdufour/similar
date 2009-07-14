b915f6dd6fbf06f33235996b39e29dee374df0c7
Nicolas R Dufour <nrdufour@gmail.com>
2009-07-14 18:59:29 -0400

    Add a very very simple process ;)

---
+++ src/process/simple.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%%% File : dummy_process.erl
+%%% Author : Nicolas R Dufour <nrdufour@gmail.com>
+%%% Created : 2009/07/14
+%%% Description :
+%%%     Simple process just saying hello and dieing.
+%%%
+%%% Copyright 2009 Nicolas R Dufour <nrdufour@gmail.com>
+%%%
+%%% This software is licensed as described in the file LICENSE, which
+%%% you should have received as part of this distribution.
+%%%-------------------------------------------------------------------
+
+-module(simple).
+-compile(export_all).
+
+start() ->
+	io:format("Hello I'm ~p and going to die soon!~n", [self()]).
+
+%%% end
