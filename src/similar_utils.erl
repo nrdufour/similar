379a87a87ac5da44ebcc4bb2c87548b6309b05bb
Nicolas R Dufour <nrdufour@gmail.com>
2010-04-27 08:33:14 -0400

    Use the same copyright notice.

--- src/similar_utils.erl
+++ src/similar_utils.erl
@@ -1,15 +1,19 @@
-%%%-------------------------------------------------------------------
-%%% File : similar_process.erl
-%%% Author : Nicolas Dufour <nrdufour@gmail.com>
-%%% Created : 2009/07/29
-%%% Description :
-%%%	Utility functions related to the simulation engine.
-%%%
-%%% Copyright 2009 Nicolas R Dufour <nrdufour@gmail.com>
-%%%
-%%% This software is licensed as described in the file LICENSE, which
-%%% you should have received as part of this distribution.
-%%%-------------------------------------------------------------------
+%% Copyright 2009-2010 Nicolas R Dufour.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% @author Nicolas R Dufour <nrdufour@gmail.com>
+%% @copyright 2009-2010 Nicolas R Dufour.
 
 -module(similar_utils).
 
