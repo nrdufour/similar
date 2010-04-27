bc2e377e09f85dae6cdc36d0274ac389d255b3c0
Nicolas R Dufour <nrdufour@gmail.com>
2010-04-27 08:54:10 -0400

    Add missing start_link/0 in the server.

--- src/similar_server.erl
+++ src/similar_server.erl
@@ -18,12 +18,17 @@
 -module(similar_server).
 -behaviour(gen_server).
 
+-export([start_link/0]).
+
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
         terminate/2, code_change/3]).
 -compile(export_all).
 
 -include("similar_data.hrl").
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %%====================================================================
 %% gen_server callbacks
